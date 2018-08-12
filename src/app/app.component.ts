dfaf47d40a9caaa37ee57b74c6ab9e67ac518be3
Ayman <ayman.khm@gmail.com>
2018-08-12 14:57:47 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+//This is a decorator, it specifies that the 'AppComponent' class is not just a plain class, but a class representing component
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Guíame';
+}
